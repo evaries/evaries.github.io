3098d90bcd42b381704388c1f15e701d087a3b6e
Evgeniy Nadtochiy <e.nadtochiy@MacBook-Air-Evgeniy.local>
2020-10-09 17:19:57 +0300

    updated

---
+++ precache-manifest.998c5b848ef1cf43810df3bd694f1a0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1510ef9cc4368a8028ded6530f409a11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b513647faeaf097774f",
+    "url": "/static/css/main.adb2518b.chunk.css"
+  },
+  {
+    "revision": "733c60f04dd9cc25200f",
+    "url": "/static/js/2.84cb5997.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.84cb5997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b513647faeaf097774f",
+    "url": "/static/js/main.7ae01d58.chunk.js"
+  },
+  {
+    "revision": "433a9e62c9870ae23b37",
+    "url": "/static/js/runtime-main.824c25ed.js"
+  },
+  {
+    "revision": "276b2ba6b79973c3215716dd829c1be1",
+    "url": "/static/media/hand1.276b2ba6.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/game-start/precache-manifest.f03da6551fb9cfb589d101190f3b4128.js"
+  "/precache-manifest.998c5b848ef1cf43810df3bd694f1a0e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/game-start/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
